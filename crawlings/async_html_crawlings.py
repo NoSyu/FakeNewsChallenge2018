8e9ff4b8179108af8cc84a2962e2b85d3b381be2
Kyungmin <sorudals@kaist.ac.kr>
2018-08-13 16:44:48 +0900

    Add html crawling file

--- crawlings/async_html_crawlings.py
+++ crawlings/async_html_crawlings.py
@@ -1,71 +1,76 @@
 import asyncio
 import traceback
+import os.path
 from typing import List
 
 import aiohttp
 import uvloop
 from openpyxl import load_workbook
-from openpyxl.worksheet import Worksheet
 
 
 class DataConfiguration:
 
-    __slots__ = ['url', 'row']
+    __slots__ = ['url', 'title', 'index']
 
-    def __init__(self, url: str, row: int) -> None:
+    def __init__(self, url: str, title: str, index: int) -> None:
         self.url = url
-        self.row = row
+        self.title = title
+        self.index = index
 
 
-class CrawledDataConfiguration:
-
-    __slots__ = ['html', 'row']
-
-    def __init__(self, html: str, row: int) -> None:
-        self.html = html
-        self.row = row
-
-
-def read_data(ws: Worksheet) -> List[DataConfiguration]:
-    data = [
-        DataConfiguration(ws[f'D{i}'].value, int(ws[f'A{i}'].value) + 2)
-        for i in range(2, ws.max_row + 1)
-    ]
-    return data
-
-async def send_request(data: DataConfiguration) -> CrawledDataConfiguration:
-    async with aiohttp.ClientSession(
-        connector=aiohttp.TCPConnector(verify_ssl=False)
-    ) as session:
-        async with session.get(data.url) as res:
-            html = await res.text()
-            return CrawledDataConfiguration(html, data.row)
-
-async def crawling_files(file_name):
+def read_data(file_name: str) -> List[DataConfiguration]:
     wb = load_workbook(file_name)
     ws = wb.worksheets[0]
-    data_configurations = read_data(ws)
+    data = [
+        DataConfiguration(
+            ws[f'D{i}'].value,
+            ws[f'B{i}'].value,
+            int(ws[f'A{i}'].value)
+        ) for i in range(2, ws.max_row + 1)
+    ]
+    wb.close()
+    return data
+
+async def send_request(
+    save_directory: str,
+    data: DataConfiguration
+) -> None:
+    try:
+        if not os.path.isfile(
+            f'./{save_directory}/{data.index}.txt'
+        ):
+            async with aiohttp.ClientSession(
+                connector=aiohttp.TCPConnector(verify_ssl=False)
+            ) as session:
+                async with session.get(data.url) as res:
+                    html = await res.text()
+                    with open(f'./{save_directory}/{data.index}.txt', 'w') as file:
+                        file.write(html)
+    except Exception as e:
+        traceback.print_exc()
+        print(data.title)
+
+async def crawling_files(file_name, save_directory):
+    data_configurations = read_data(file_name)
     requests = [
-        send_request(data_configuration)
+        send_request(save_directory, data_configuration)
         for data_configuration in data_configurations
     ]
-    crawled_data_configurations = await asyncio.gather(*requests[:10])
-    for crawled_data_configuration in crawled_data_configurations:
-        ws.cell(
-            row=crawled_data_configuration.row,
-            column=5,
-            value=crawled_data_configuration.html
-        )
-    wb.save(file_name)
+    for i in range(0, len(requests), 10):
+        await asyncio.gather(*requests[i: i+10])
 
-async def main(loop):
+async def main():
     try:
         file_names = [
             'competition_stances_uniqueUrl.xlsx',
             'train_stances_uniqueUrl.xlsx'
         ]
-        for file_name in file_names:
-            await crawling_files(file_name)
+        save_directories = [
+            'competition_htmls',
+            'train_htmls'
+        ]
+        for index, file_name in enumerate(file_names):
+            await crawling_files(file_name, save_directories[index])
     except Exception as e:
         traceback.print_stack()
         print('-------------------------------')
@@ -75,5 +80,5 @@ async def main(loop):
 if __name__ == '__main__':
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(loop))
+    loop.run_until_complete(main())
     loop.close()
